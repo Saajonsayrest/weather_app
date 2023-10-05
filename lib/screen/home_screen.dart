19209f7d61ee0f31692dc8481a8104eb377ebfd6
Saajonsayrest <sajonshrestha5@gmail.com>
2023-10-05 22:59:27 +0545

    complete ui

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,200 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: PreferredSize(
+        preferredSize: Size(width, 25),
+        child: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          systemOverlayStyle:
+              const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+        child: SizedBox(
+          height: height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: const AlignmentDirectional(3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(-3, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.deepPurple),
+                ),
+              ),
+              Align(
+                alignment: const AlignmentDirectional(0, -1.8),
+                child: Container(
+                  height: 300,
+                  width: 500,
+                  decoration: const BoxDecoration(color: Color(0xFFFFAB40)),
+                ),
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 120, sigmaY: 120),
+                child: Container(
+                  decoration: const BoxDecoration(color: Colors.transparent),
+                ),
+              ),
+              SizedBox(
+                width: width,
+                height: height,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      '📍 Kathmandu',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w300),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'Good Morning',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25),
+                    ),
+                    Center(child: Image.asset('assets/4.png', height: 300)),
+                    const Center(
+                      child: Text(
+                        '21℃',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 55,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    const Center(
+                      child: Text(
+                        'THUNDERSTORM',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    const Center(
+                      child: Text(
+                        'Friday 16 • 09.41am',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        WeatherFeatures(
+                          image: 'assets/1.png',
+                          title: 'Sunrise',
+                          time: '5:34am',
+                        ),
+                        WeatherFeatures(
+                          image: 'assets/4.png',
+                          title: 'Sunset     ',
+                          time: '5:34am',
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Divider(
+                        color: Colors.grey,
+                        thickness: 0.4,
+                        indent: 10,
+                      ),
+                    ),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        WeatherFeatures(
+                          image: 'assets/13.png',
+                          title: 'Temp Max.',
+                          time: '12℃',
+                        ),
+                        WeatherFeatures(
+                          image: 'assets/14.png',
+                          title: 'Temp Min.',
+                          time: '5℃',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WeatherFeatures extends StatelessWidget {
+  const WeatherFeatures({
+    super.key,
+    required this.image,
+    required this.title,
+    this.time,
+    this.temperature,
+  });
+
+  final String image;
+  final String title;
+  final String? time;
+  final String? temperature;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const SizedBox(width: 10),
+        Image.asset(image, scale: 8),
+        const SizedBox(width: 5),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w300),
+            ),
+            const SizedBox(height: 3),
+            Text(
+              time ?? temperature ?? '',
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.w700),
+            ),
+          ],
+        ),
+        const SizedBox(width: 10),
+      ],
+    );
   }
 }
